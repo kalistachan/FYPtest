bd5d7611d77bda83b777f0fc5d45a324b640599c
Ming Yan <LMY@hotmail.sg>
2019-04-07 20:52:10 +0800

    added Profile Fragment

---
+++ app/src/main/java/com/example/fyptest/fragments/ProfileFragment.java
@@ -0,0 +1,20 @@
+package com.example.fyptest.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fyptest.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
