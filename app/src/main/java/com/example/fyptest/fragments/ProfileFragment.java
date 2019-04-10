391db5625c4673589f73e65b6c10d223b7c67ac3
mingyan <LMY@hotmail.sg>
2019-04-10 23:22:25 +0800

    Updated by LMY @ 11:22pm 10/4/2019

--- app/src/main/java/com/example/fyptest/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/fyptest/fragments/ProfileFragment.java
@@ -1,19 +1,15 @@
 package com.example.fyptest.fragments;
 
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 import com.example.fyptest.R;
 
 public class ProfileFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,7 +19,6 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
     }
 }
