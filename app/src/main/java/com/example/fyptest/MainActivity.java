0de4b15e841869b70682c674aca751becf4dac0f
Jan Mathew <loveless_wawa@hotmail.com>
2019-04-11 11:55:06 +0800

    added a floating nav_drawer to MainActivity like FINALLY fuc

--- app/src/main/java/com/example/fyptest/MainActivity.java
+++ app/src/main/java/com/example/fyptest/MainActivity.java
@@ -20,6 +20,16 @@ import com.example.fyptest.fragments.GroupFragment;
 import com.example.fyptest.fragments.ProfileFragment;
 import com.example.fyptest.fragments.PurchaseFragment;
 import com.google.firebase.database.DatabaseReference;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -62,6 +72,51 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences prefs = getSharedPreferences("IDs", MODE_PRIVATE);
         String id = prefs.getString("userID", "UNKNOWN");
         Toast.makeText(this, id, Toast.LENGTH_LONG).show();
+
+//nav_drawer components-----------------------------------------------------------------------------
+
+        //account_header
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.color.colorPrimaryDark)
+                .addProfiles(
+                        new ProfileDrawerItem().withName(R.string.account_header_user).withEmail(R.string.account_header_email)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        return false;
+                    }
+                })
+                .build();
+        //account_header
+
+        new DrawerBuilder().withActivity(this);
+
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem item_main = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_main);
+        SecondaryDrawerItem item_secondary = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_secondary);
+
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withSelectedItem(-1)
+                .withActivity(this)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        item_main,
+                        new DividerDrawerItem(),
+                        item_secondary,
+                        new SecondaryDrawerItem()
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        //do funky stuff with clicked items :D
+                        return false;
+                    }
+                })
+                .build();
+//nav_drawer components-----------------------------------------------------------------------------
 
 
     }
