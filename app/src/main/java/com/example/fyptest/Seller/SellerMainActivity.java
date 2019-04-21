96aafe5444060c419f8771759da71e862485860c
mingyan <LMY@hotmail.sg>
2019-04-21 15:05:57 +0800

    @updated purchases view xml design @1352hrs 21/04/2019

--- app/src/main/java/com/example/fyptest/Seller/SellerMainActivity.java
+++ app/src/main/java/com/example/fyptest/Seller/SellerMainActivity.java
@@ -1,15 +1,26 @@
 package com.example.fyptest.Seller;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.fyptest.R;
 
 public class SellerMainActivity extends AppCompatActivity {
 
+    SharedPreferences preferences;
+    String userIdentity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_main);
+
+        preferences = getSharedPreferences("IDs", MODE_PRIVATE);
+        userIdentity = preferences.getString("userID", "UNKNOWN");
+
+        Toast.makeText(this, userIdentity, Toast.LENGTH_LONG).show();
     }
 }
