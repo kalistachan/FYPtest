b7c436559a46db3a7f4b4a07862968e69726c347
mingyan <LMY@hotmail.sg>
2019-05-01 16:05:07 +0800

    12312312312312

--- app/src/main/java/com/example/fyptest/loginActivity.java
+++ app/src/main/java/com/example/fyptest/loginActivity.java
@@ -19,6 +19,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.security.MessageDigest;
+
 public class loginActivity extends AppCompatActivity {
 
     EditText emailEntered, passwordEntered;
