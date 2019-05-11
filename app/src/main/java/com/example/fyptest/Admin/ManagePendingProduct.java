158b2d772900b8b5641deeed6cd5945ef3563f01
Ming Yan <LMY@hotmail.sg>
2019-05-11 15:26:05 +0800

    @mingyan update @3:20PM Saturday 11/05/2019

--- app/src/main/java/com/example/fyptest/Admin/ManagePendingProduct.java
+++ app/src/main/java/com/example/fyptest/Admin/ManagePendingProduct.java
@@ -202,9 +202,11 @@ public class ManagePendingProduct extends Fragment {
                     if (dataSnapshot.hasChild("pro_freeShippingAt")) {
                         String freeShipping = "$" + dataSnapshot.child("pro_freeShippingAt").getValue().toString();
                         checkBoxFreeShipment.setChecked(true);
+                        editTextFreeShipCondition.setEnabled(false);
                         editTextFreeShipCondition.setText(freeShipping);
                     } else if (!dataSnapshot.hasChild("pro_freeShippingAt")) {
                         checkBoxFreeShipment.setChecked(false);
+                        editTextFreeShipCondition.setEnabled(false);
                     }
                 }
             }
